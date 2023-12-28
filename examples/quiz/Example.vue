d25aaedadbe014ef044e99125f45cb10bdb02a41
dtol95 <80629265+dtol95@users.noreply.github.com>
2023-12-28 03:03:02 -0600

    Update Example.vue

--- examples/quiz/Example.vue
+++ examples/quiz/Example.vue
@@ -1,4 +1,3 @@
-// Create and setup your form here
  
 <template>
   <div>
@@ -60,11 +59,6 @@
 </template>
 
 <script>
-  /*
-    Copyright (c) 2020 - present, DITDOT Ltd. - MIT Licence
-    https://www.ditdot.hr/en
-  */
-
   // Import necessary components and classes
   import FlowForm from '../../src/components/FlowForm.vue'
   import QuestionModel, { QuestionType, ChoiceOption, LinkOption } from '../../src/models/QuestionModel'
@@ -81,7 +75,7 @@
         submitted: false,
         completed: false,
         score: 0, 
-        total: 15, 
+        total: 45, 
         time: 0,
         formattedTime: '',
         answers: {
@@ -100,15 +94,139 @@
           html_13: '4', // Creator of Royal Rumble competition 
           html_14: '2', // Royal mapper of the 2022 year 
           html_15: '3', // Royal photographer 
+          question_16: [
+            "lucian____",
+            "lucian",
+            "Lucian",
+          ],
+          question_17: [
+            "Dog..",
+            "Dog",
+          ],
+          question_18: [
+            "Brimarine",
+            "brimarine",
+          ],
+          question_19: [
+            "yungweeb",
+            "Yungweeb",
+          ],
+          question_20: [
+            "Kelvawesome2070",
+            "kelvawesome2070",
+            "Kelvawesome",
+          ],
+          question_21: [
+            "ya___",
+            "Ya___",
+            "ya",
+          ],
+          question_22: [
+            "Sombot",
+            "sombot",
+          ],
+          question_23: [
+            "RafTorTV",
+            "raftortv",
+            "RafTor",
+          ],
+          question_24: [
+            "Tchak77",
+            "tchak77",
+          ],
+          question_25: [
+            "Youpikaii",
+            "youpikaii",
+          ],
+          question_26: [
+            "kickz21",
+            "Kickz21",
+          ],
+          question_27: [
+            "TRASHER712",
+            "trasher712",
+          ],
+          question_28: [
+            "Rap10tor",
+            "rap10tor",
+          ],
+          question_29: [
+            "YourOnlyHope7",
+            "youronlyhope7",
+          ],
+          question_30: [
+            "Raway..",
+            "raway",
+          ],
+          question_31: [
+            "Moonub",
+            "moonub",
+          ],
+          question_32: [
+            "Surgetale",
+            "surgetale",
+          ],
+          question_33: [
+            "MrPaxi",
+            "mrpaxi",
+          ],
+          question_34: [
+            "YanMan",
+            "yanman",
+          ],
+          question_35: [
+            "Stormynoobgamer",
+            "stormynoobgamer",
+          ],
+          question_36: [
+            "Hey-E",
+            "hey-e",
+          ],
+          question_37: [
+            "Mabiere",
+            "mabiere",
+          ],
+          question_38: [
+            "Hayiom..",
+            "hayiom",
+          ],
+          question_39: [
+            "SkyRay746",
+            "skyray746",
+          ],
+          question_40: [
+            "Baupro32",
+            "baupro32",
+          ],
+          question_41: [
+            "LittleKryli___",
+            "littlekryli___",
+          ],
+          question_42: [
+            "betabr",
+            "Betabr",
+          ],
+          question_43: [
+            "Morgus_TM",
+            "morgus_tm",
+          ],
+          question_44: [
+            "Ideallinie13",
+            "ideallinie13",
+          ],
+          question_45: [
+            "Tona.",
+            "tona",
+          ],
         },
         language: new LanguageModel(),
         // Create question list with QuestionModel instances
         questions: [      
           new QuestionModel({
             id: 'start',
-            tagline: '15 questions',
+            tagline: '45 questions',
             title: 'How much do you know about Royal?',
-            content: 'Test how well you know Royal history in the first quiz section.',
+            content: 'Let\'s start with some multiple choice questoins',
             type: QuestionType.SectionBreak,
             required: true,
           }),
@@ -244,7 +362,7 @@
           }),
           new QuestionModel({
             id: 'html_6',
-            title: 'Team that won SR most of the times?',
+            title: 'Team that won the most Super Royals?',
             helpText: '',
             type: QuestionType.MultipleChoice,
             required: true,
@@ -503,10 +621,280 @@
             ]
           }),
           new QuestionModel({
-            title: 'Excellent! You are halfway through.',
-            content: 'Lets see how well you know your maps.',
+            title: 'Excellent! Now for this part you will type out your answers, don\'t stress about capitalization',
+            content: 'Names will not be used more than once, so if you have guessed someone already it will probably not be them if you are correct.',
             type: QuestionType.SectionBreak
-          })
+          }),
+          new QuestionModel({
+            id: 'question_16',
+            tagline: '',
+            title: '4th place andy. Tchak\'s main simp.',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_17',
+            tagline: '',
+            title: 'Appreciates Moonub SadgeClap, made classic royal server',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_18',
+            tagline: '',
+            title: 'Main writer of the Royal community letter',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_19',
+            tagline: '',
+            title: 'This guy is always getting carried',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_20',
+            tagline: '',
+            title: 'First player to get the master crown fully on starter access',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_21',
+            tagline: '',
+            title: 'Has one of the shortest nicknames of the entire Royal community',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_22',
+            tagline: '',
+            title: 'The best Royal youngster',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_23',
+            tagline: '',
+            title: 'The one and only RRR creator/host',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_24',
+            tagline: '',
+            title: 'Announced retirement from Royal in 2022 but then made a comeback and started playing with one of the best teams',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_25',
+            tagline: '',
+            title: 'Toxic "LAYS" guy, but his English can make anyone smile',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_26',
+            tagline: '',
+            title: 'Chriso\'s rival',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_27',
+            tagline: '',
+            title: 'German pipes legend',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_28',
+            tagline: '',
+            title: 'RafTor\'s "alt" account',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_29',
+            tagline: '',
+            title: 'Fede 007 guy',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_30',
+            tagline: '',
+            title: 'A pro creator of Royal tutorials',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_31',
+            tagline: '',
+            title: 'Who???',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_32',
+            tagline: '',
+            title: 'Banana guy, who shared his advice on how to join the best team',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_33',
+            tagline: '',
+            title: 'A real Foxdrive friend',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_34',
+            tagline: '',
+            title: 'Likes to drive "inside of the tubes" even made a sequel of those maps',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_35',
+            tagline: '',
+            title: 'One of the PRIME-b players with a very long name',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_36',
+            tagline: '',
+            title: 'Also known as EAWS2929',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_37',
+            tagline: '',
+            title: 'EZ member who would like your beer',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_38',
+            tagline: '',
+            title: 'Used to have no bitches, but no friends',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_39',
+            tagline: '',
+            title: 'Prolific mapper with 1 SR win and 0 normal wins',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_40',
+            tagline: '',
+            title: 'Argentine RNC champion',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_41',
+            tagline: '',
+            title: 'Little guy who got returned to Royal because of Rick streams',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_42',
+            tagline: '',
+            title: '!jump',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_43',
+            tagline: '',
+            title: 'Old Royal boomer',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_44',
+            tagline: '',
+            title: 'The most active PRIME-ISSUE player',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
+          new QuestionModel({
+            id: 'question_45',
+            tagline: '',
+            title: 'NADEO stuff person who is trying his best for us',
+            type: QuestionType.Text,
+            required: true,
+            placeholder: 'Type your answer here...',
+          }),
+
         ]
       }
     },
@@ -525,20 +913,27 @@
       calculateScore() {
         this.questions.forEach(question => {
           if (question.type !== QuestionType.SectionBreak) {
-            let answer = question.answer
-            if (typeof answer === 'object') {
-              answer.sort((a, b) => a - b)
+            let answer = question.answer;
+
+            if (typeof answer === 'string') {
+              // Text-based question
+              let correctAnswers = this.answers[question.id];
+
+              // Check if the user's answer matches any of the correct variations
+              if (correctAnswers.includes(answer.trim())) {
+                this.score++;
+              }
+            } else if (typeof answer === 'object') {
+              // Multiple-choice question
+              answer.sort((a, b) => a - b);
 
               if (this.arrayEquals(answer, this.answers[question.id])) {
-                this.score++
+                this.score++;
               }
             }
-            if (answer == this.answers[question.id]) {
-              this.score++
-            }
           }
-        })
-      }, 
+        });
+      },
 
       onQuizSubmit() {
         // Set `submitted` to true so the form knows not to allow back/forward
